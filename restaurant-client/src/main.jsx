7e30901493770136e3a011190971ac83047f4a45
Shahriar Ahmed <shahriarabiddut@gmail.com>
2025-01-01 15:50:45 +0600

    Client: Firebase and Provider Setup!

--- restaurant-client/src/main.jsx
+++ restaurant-client/src/main.jsx
@@ -6,13 +6,16 @@ import {
 import './index.css';
 import router from './Routes/router.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>,
 )
---
+++ restaurant-client/src/providers/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+const auth = getAuth(app);
+const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+    const authInfo = {
+        user,loading
+    }
+  return (
+    <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
